49bbbc1b51fae9e39d60d4c1e49872d93c91bd64
aviad omeisi <aviadomeisi@aviads-MacBook-Pro.local>
2018-06-24 17:43:53 -0400

    finished the Modal section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,12 @@
+import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
-import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
-var stickyHeader = new StickyHeader;
+var stickyHeader = new StickyHeader();
+var modal = new Modal();
+
 new RevealOnScroll($('.features-item'), '85%');
 new RevealOnScroll($('.testimonials'), '60%');
