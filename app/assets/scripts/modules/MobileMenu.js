e249f3926a5e4a2c9bb8ee386dcc533c64d7873c
aviad omeisi <aviadomeisi@aviads-MacBook-Pro.local>
2018-06-23 20:10:23 -0400

    complited the header

---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,24 @@
+import $ from 'jquery';
+
+class MobileMenu {
+	constructor () {
+		this.siteHeader = $(".site-header");
+		this.menuIcon = $(".site-header__menu-icon");/*doing that first*/
+		this.menuContent = $(".site-header__menu-content");
+		this.events();
+	}
+
+	events() {
+		this.menuIcon.click(this.toggleTheMenu.bind(this));
+	}
+
+	toggleTheMenu(){
+		this.menuContent.toggleClass("site-header__menu-content__is-visible");
+		this.siteHeader.toggleClass("site-header__is-expanded");
+		this.menuIcon.toggleClass("site-header__menu-icon__close-x");
+	}
+}
+
+
+
+export default MobileMenu;
