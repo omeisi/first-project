85add95cdac6489f344449d9721a6b7228afe342
aviad omeisi <aviadomeisi@aviads-MacBook-Pro.local>
2018-06-24 00:11:47 -0400

    finished the stickyheader

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
+var stickyHeader = new StickyHeader;
 new RevealOnScroll($('.features-item'), '85%');
 new RevealOnScroll($('.testimonials'), '60%');
